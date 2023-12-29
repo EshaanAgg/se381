375975026ddb4c3f483f0a07a639a68e8b6f45cc
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-12-30 00:21:13 +0530

    lab1: add serve and make all the requests parallely

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Software Engineering
 
-This repository contains all my scripts for the course CSE381: Software Engineering.
+This repository contains all my scripts for the course `CSE381: Software Engineering`.
 
 ## Lab 1
 
-You need to have a `OpenAI` API key to run the script. Please save the same in `.env` in the root of this directory. Also then just paste the lines for which you want to generate the stories in [data.txt](./lab1/data.txt).
+You need to have a `OpenAI` API key to run the script. Please save the same in `.env` in the root of this directory (use the field name as `OPENAI_API_KEY`).
 
-After completing the setup, run the following:
+You need to paste the lines for which you want to generate the stories in [data.txt](./lab1/data.txt). After the same is done, you can run the following:
 
 ```bash
 npm install
 npm run lab1
 ```
 
-The generated stories would be present in [out.json](./lab1/out.json). To get the output as a table, you need to open the [index.html](./lab1/index.html) file with the [`LiveServer`](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) extension in VSCode. You can then copy and paste the generated table into an Excel.
+The generated stories would be present in [out.json](./lab1/out.json). To get the output as a table, you need to open the [index.html](./lab1/index.html) file. The same would be served on `http://localhost:3000` on your machine directly when you run the above command with the help of [`serve`](https://github.com/vercel/serve). You can copy and paste the table directly to MS Excel or Google Sheets to get the formatted output.
--- lab1/index.js
+++ lab1/index.js
@@ -1,18 +1,17 @@
 import 'dotenv/config';
 import OpenAI from 'openai';
 import fs from 'fs';
-import sampleResponse from './sample.json' assert { type: 'json' };
 
 const generatePrompt = (input) => {
   return `
-I want to extract design elements from a set of user stories. I need to generate the classes, attributes and relatioships from the provided user story. The input would be in form of a sentence, and the output in the form of JSON.
+I want to extract design elements from a set of user stories. I need to generate the classes, attributes and relationships from the provided user story. The input would be in form of a sentence, and the output in the form of JSON.
 
 Please only respond with valid JSON and nothing else. Also keep the design of the story simple, and focus only on the requirements from the given input.
 
 Example Input: "As a faculty member, I want to mark attendance of students so that I can track their regularity."
 Example Output:
 """
-${JSON.stringify(sampleResponse, null, 2)}
+${fs.readFileSync('lab1/sample.json', 'utf-8')}
 """
 
 Your Input: ${input}
@@ -34,22 +33,23 @@ const generateStory = async (input) => {
     model: 'gpt-3.5-turbo',
   });
 
-  return JSON.parse(chatCompletion.choices[0].message.content);
+  return {
+    input,
+    story: JSON.parse(chatCompletion.choices[0].message.content),
+  };
 };
 
 const main = async () => {
-  const lines = fs.readFileSync('lab1/data.txt', 'utf-8').split('\n');
+  const requestPromises = fs
+    .readFileSync('lab1/data.txt', 'utf-8')
+    .split('\n')
+    .map((line) => generateStory(line.trim()));
 
-  const output = [];
-  for (const line of lines) {
-    const story = await generateStory(line);
-    output.push({
-      input: line,
-      story,
-    });
-  }
+  const responses = await Promise.all(requestPromises);
 
-  fs.writeFileSync('lab1/out.json', JSON.stringify(output, null, 2));
+  fs.writeFileSync('lab1/out.json', JSON.stringify(responses, null, 2));
+
+  console.log('All the data has been generated!');
 };
 
 main();
