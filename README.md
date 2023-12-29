10ee156aaa16438383649f0014f555e06038e48b
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-12-29 16:52:15 +0530

    init: lab1

---
+++ README.md
@@ -0,0 +1,16 @@
+# Software Engineering
+
+This repository contains all my scripts for the course CSE381: Software Engineering.
+
+## Lab 1
+
+You need to have a `OpenAI` API key to run the script. Please save the same in `.env` in the root of this directory. Also then just paste the lines for which you want to generate the stories in [data.txt](./lab1/data.txt).
+
+After completing the setup, run the following:
+
+```bash
+npm install
+node lab1/index.js
+```
+
+The generated stories would be present in [out.json](./lab1/out.json).
---
+++ lab1/index.js
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import OpenAI from "openai";
+import fs from "fs";
+import sampleResponse from "./sample.json" with { type: "json" };
+
+const generatePrompt = (input) => {
+	return `
+I want to extract design elements from a set of user stories. I need to generate the classes, attributes and relatioships from the provided user story. The input would be in form of a sentence, and the output in the form of JSON.
+
+Please only respond with valid JSON and nothing else. Also keep the design of the story simple, and focus only on the requirements from the given input.
+
+Example Input: "As a faculty member, I want to mark attendance of students so that I can track their regularity."
+Example Output:
+"""
+${JSON.stringify(sampleResponse, null, 2)}
+"""
+
+Your Input: ${input}
+`;
+};
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const generateStory = async (input) => {
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: "user",
+				content: generatePrompt(input),
+			},
+		],
+		model: "gpt-3.5-turbo",
+	});
+
+	return JSON.parse(chatCompletion.choices[0].message.content);
+};
+
+const main = async () => {
+	const lines = fs.readFileSync("lab1/data.txt", "utf-8").split("\n");
+
+	const output = [];
+	for (const line of lines) {
+		const story = await generateStory(line);
+		output.push(story);
+	}
+
+	fs.writeFileSync("lab1/out.json", JSON.stringify(output, null, 2));
+};
+
+main();
